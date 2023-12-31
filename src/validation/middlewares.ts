69d19c171d1ba0fc25eb146b0b2c7064d276f296
Andrey Allyson <andreyuser47@gmail.com>
2023-12-31 19:51:33 -0300

    doc: added documentation for validation middleware factories

--- src/validation/middlewares.ts
+++ src/validation/middlewares.ts
@@ -13,6 +13,12 @@ export function createDTO<S extends ZodRawShape>(shape: S) {
   };
 }
 
+/**
+ * Creates a express middleware that validates the request body with `type` schema
+ * 
+ * @param type Schema to that validate the request body
+ * @returns A express request handler that thwons if request body is invalid
+ */
 export function valid<B>(type: ZodType | ObjectWithSchema): e.RequestHandler {
   const schema = 'schema' in type ? type.schema : type;
 
@@ -23,6 +29,12 @@ export function valid<B>(type: ZodType | ObjectWithSchema): e.RequestHandler {
   }
 }
 
+/**
+ * Create a express middleware that validates request query with `type` shema
+ * 
+ * @param type Shema to validate the request query
+ * @returns A express request handler that thowns if request query is invalid
+ */
 valid.query = function(type: ZodType | ObjectWithSchema): e.RequestHandler {
   const schema = 'schema' in type ? type.schema : type;
 
